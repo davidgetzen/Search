787d97d3326dfac9877d6beca9aaabf20590f3c7
davidgetzen <98428144+davidgetzen@users.noreply.github.com>
2022-04-25 21:24:47 -0400

    words-to-counts

--- Indexer.py
+++ Indexer.py
@@ -8,6 +8,7 @@ from nltk.stem import PorterStemmer
 STOP_WORDS = stopwords.words('english')
 the_stemmer = PorterStemmer()
 
+
 class Indexer:
     def __init__(self, data_path, title_path):
         self.file_path = data_path
@@ -22,22 +23,27 @@ class Indexer:
 
         for page in all_pages:
 
-            page_id = int(page.find("id").text) # int(page.find("id"))
+            page_id = int(page.find("id").text)  # int(page.find("id"))
             page_title = page.find("title").text.strip()
             ids_to_titles[page_id] = page_title
 
             page_text = page.find("text").text.lower()
             ids_to_words[page_id] = self.get_page_words(page_text)
 
+        corpus = self.get_corpus(ids_to_words)
+        ids_to_counts = {}
+        for word in corpus:
+            ids_to_counts[word] = corpus.count(word)
+
         file_io.write_title_file(self.title_path, ids_to_titles)
-    
+
     def tokenize_text(self, text):
         n_regex = '''\[\[[^\[]+?\]\]|[a-zA-Z0-9]+'[a-zA-Z0-9]+|[a-zA-Z0-9]+'''
         text_tokens = re.findall(n_regex, text)
         return text_tokens
 
     def remove_stop_words(self, words):
-        return [word for word in words if word not in STOP_WORDS]   
+        return [word for word in words if word not in STOP_WORDS]
 
     def stem_words(self, words):
         return [the_stemmer.stem(word) for word in words]
@@ -47,3 +53,11 @@ class Indexer:
         page_without_stop_words = self.remove_stop_words(page_tokens)
         page_with_stemmed_words = self.stem_words(page_without_stop_words)
         return page_with_stemmed_words
+
+    def get_corpus(self, ids_dict):
+        corpus = []
+        for words in ids_dict.values():
+            for word in words:
+                if word not in corpus:
+                    corpus.append(word)
+        return corpus
