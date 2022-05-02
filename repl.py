c7786b076f2497e69385266fb87c24dad124e634
Ayma-n <aymanbt14@gmail.com>
2022-05-02 17:24:34 -0400

    some slight modif + added pagerankwiki

--- repl.py
+++ repl.py
@@ -2,20 +2,8 @@ from index import Indexer
 from query import Querier
 
 if __name__ == "__main__":
-<<<<<<< Updated upstream
-    indexer = Indexer("wikis/MedWiki.xml", "title_file.txt", "docs_file.txt", "words_file.txt")
-    indexer.parse_xml()
-    #querier = Querier("title_file.txt", "docs_file.txt", "words_file.txt", True)
-    #querier.start_querying("lycée lyautey")
-    
-    #while True:
-        #user_text = input("Please enter your query (or type :quit to leave the program): ")
-        #if user_text == ":quit":
-            #break
-        #print(user_text.upper()) # This is where we should call the helper for the querier.
-=======
-    indexer = index("wikis/SmallWiki.xml", "title_file.txt",
-                    "words_file.txt", "docs_file.txt")
+    indexer = Indexer("wikis/PageRankWiki.xml", "title_file.txt",
+                    "docs_file.txt", "words_file.txt")
     indexer.parse_xml()
     # querier = Querier("title_file.txt", "docs_file.txt", "words_file.txt", True)
     # querier.start_querying("lycée lyautey")
@@ -25,4 +13,3 @@ if __name__ == "__main__":
     # if user_text == ":q":
     #     break
     # print(user_text.upper()) # This is where we should call the helper for the querier.
->>>>>>> Stashed changes
